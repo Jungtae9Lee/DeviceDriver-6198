d34737742c00b5a5a826e0af699300e5d5597f45
jungtae <clickmax85@gmail.com>
2023-06-05 15:54:10 +0900

    read 구현 완료

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -6,8 +6,38 @@
 using namespace testing;
 using namespace std;
 
-TEST(TestCaseName, TestFiveRead) {
-	// 실패하는 테스트
+class MockDevice : public FlashMemoryDevice{
+public:
+	MOCK_METHOD(unsigned char, read, (long address), (override));
+	MOCK_METHOD(void, write, (long address,unsigned char data), (override));
+};
 
-  EXPECT_THAT(1, Eq(1));
+TEST(TestCaseName, TestnameFiveRead) {
+	
+	// 대역
+	MockDevice mock_device;
+
+	EXPECT_CALL(mock_device, read).Times(5);
+	DeviceDriver driver(&mock_device);
+	driver.read(0xA);
+
+  //EXPECT_THAT(1, Eq(1));
+}
+
+TEST(TestCaseName, Exception) {
+	MockDevice mock_device;
+	// 스터빙 4버은 잘 읽히는데 5번째는 다른값이 읽힐때
+
+	EXPECT_CALL(mock_device, read(0x2))
+		.WillOnce(Return(0x7))
+		.WillOnce(Return(0x7))
+		.WillOnce(Return(0x7))
+		.WillOnce(Return(0x7))
+		.WillOnce(Return(0x3));
+
+	DeviceDriver driver(&mock_device);
+	EXPECT_THROW(driver.read(0x2), exception);
+	
+
+	//EXPECT_THAT(1, Eq(1));
 }
--- TDD10_DD/DeviceDriver.cpp
+++ TDD10_DD/DeviceDriver.cpp
@@ -1,12 +1,23 @@
 #include "DeviceDriver.h"
+#include <exception>
+using namespace std;
 
+const int MAX_CNT =  4;
 DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware) : m_hardware(hardware)
 {}
 
 int DeviceDriver::read(long address)
 {
     // TODO: implement this method properly
-    return (int)(m_hardware->read(address));
+    int result = (int)(m_hardware->read(address));
+
+    for (int i = 0; i < MAX_CNT; ++i)   {
+        int nextResult = (int)(m_hardware->read(address));
+        if (result == nextResult) continue;
+        throw exception("Exception!!");
+    }
+
+    return result;
 }
 
 void DeviceDriver::write(long address, int data)
